8bdd777af52e84cc733d3291b90d69323960558f
FengYuan027 <bettyfengyuan@msn.com>
2018-03-18 18:36:22 -0700

    Update TestConnection.py

--- TestConnection.py
+++ TestConnection.py
@@ -10,62 +10,58 @@ def SingleHostIperfTest(net)
 
     hostName = 'h1'
 	
-	host = net.getHostByName(hostName)
+    host = net.getHostByName(hostName)
 	
-	host.sendCmd('%s -s -p %s > %s/iperf_server_%s_single.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName))
+    host.sendCmd('%s -s -p %s > %s/iperf_server_%s_single.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName))
 	
-	clientNames = []
+    clientNames = []
 	
-	for i in range(2, 100, 2)
+    for i in range(2, 100, 2)
 	
-	    clientNames.append('h%s' %i)
+	clientNames.append('h%s' %i)
 		
     BeginIperfTest(net, hostName, clientNames)
 	
-	host.cmd('kill $!')
+    host.cmd('kill $!')
 	
 def TwoHostIperfTest(net)
 
     hostName1 = 'h1'
 	
-	hostName2 = 'h100'
+    hostName2 = 'h100'
 	
-	host1 = net.getHostByName(hostName1)
+    host1 = net.getHostByName(hostName1)
+
+    host1.sendCmd('%s -s -p %s > %s/iperf_server_%s_two.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName1))
 	
-	host1.sendCmd('%s -s -p %s > %s/iperf_server_%s_two.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName1))
+    host2 = net.getHostByName(hostName2)
 	
-	host2 = net.getHostByName(hostName2)
+    host2.sendCmd('%s -s -p %s > %s/iperf_server_%s_two.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName2))
 	
-	host2.sendCmd('%s -s -p %s > %s/iperf_server_%s_two.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName2))
+    clientNames1 = []
 	
-	clientNames1 = []
+    clientNames2 = []
 	
-	clientNames2 = []
+    for i in range(3, 99, 2)
 	
-	for i in range(3, 99, 2)
-	
-	    clientNames1.append('h%s' %i)
+	clientNames1.append('h%s' %i)
 		
-		clientNames2.append('h%s' %i-1)
+	clientNames2.append('h%s' %i-1)
 		
     BeginIperfTest(net, hostName1, clientNames1)
 	
-	BeginIperfTest(net, hostName2, clientNames2)
+    BeginIperfTest(net, hostName2, clientNames2)
 	
-	host1.cmd('kill $!')
+    host1.cmd('kill $!')
 	
-	host2.cmd('kill $!')
+    host2.cmd('kill $!')
 
 
 def BeginIperfTest(net, hostName, clientNames)
-
-    host = net.getHostByName(hostName)
 	
-	host.sendCmd('%s -s -p %s > %s/iperf_server_%s.txt &' %(IPERF_PATH, IPERF_PORT, IPERF_SERVER_DIRECTORY, hostName))
-	
-	for clientName in clientNames
+    for clientName in clientNames
 
-	    client = net.getHostByName(clientName)
+        client = net.getHostByName(clientName)
 		
-		h.sendCmd('%s -c %s -p %s -t %d -i 1 -yc > %s/iperf_client_%s.txt &' %(IPERF_PATH, hostName, IPERF_PORT, IPERF_CLIENT_T, IPERF_CLIENT_DIRECTORY, clientName))
+	client.sendCmd('%s -c %s -p %s -t %d -i 1 -yc > %s/iperf_client_%s.txt &' %(IPERF_PATH, hostName, IPERF_PORT, IPERF_CLIENT_T, IPERF_CLIENT_DIRECTORY, clientName))
 
